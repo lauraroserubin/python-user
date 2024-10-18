f1f823af52b0f8202aca29a356a662a92453d991
lauraroserubin <lauraroserubin@gmail.com>
2024-10-18 14:29:55 +0530

    basic input

---
+++ python user.py
@@ -0,0 +1,11 @@
+'''
+Author: Laura Rose Rubin
+date:18-10-2024
+Description:program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age
+'''
+
+
+
+name=input("enter your name")
+age=input("enter your age")
+print("Hello,",name,"! you are ",age,"years old")
